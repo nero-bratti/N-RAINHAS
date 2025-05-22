663ca02030b7f910c7e90b9d2faf7033ae8cacf4
Maria Eduarda Weiland Machado Bratti <weiland.maria@edu.pucrs.br>
2025-05-21 22:12:15 -0300

    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        Queens queens = new Queens(2,2);
-        queens.findAllPossible();
+        Queens q = new Queens(1);
+        new Queens(2);
+        new Queens(3);
+        new Queens(4);
+        new Queens(5);
+        new Queens(6);
+        new Queens(7);
+        new Queens(8);
+        new Queens(9);
+        new Queens(10);
+        new Queens(11);
     }
 
 }
